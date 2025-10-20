0842c37a387f6e03a4add2d244d21c3cb40cc1eb
tobicodesometimes <93890865+tobicodesometimes@users.noreply.github.com>
2025-10-20 11:36:01 -0400

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,1 +1,26 @@
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+dotenv.config();
 
+const connectionString = process.env.MONGODB_URI
+
+const client = new MongoClient(connectionString);
+
+let db;
+
+export async function getDB() {
+  if (!db) {
+    await client.connect();
+    console.log('Connected to MongoDB')
+    db = client.db(process.env.DB_NAME);
+  }
+  return db;
+}
+
+export function col(db) {
+  return {
+    series: db.collection("series"),
+    volumes: db.collection("volumes"),
+    reviews: db.collection("reviews")
+  };
+}
